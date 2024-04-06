d03a45bada6ebf85b4f7a086de5b8a8668bdfe61
PC <mixail.ermin33@gmail.com>
2024-04-06 16:58:44 +0300

    receive msg

--- backend-bot/index.js
+++ backend-bot/index.js
@@ -1,1 +1,14 @@
-const token = "dsfsdf"
+const TelegramBot = require('node-telegram-bot-api');
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '6918740409:AAEqqEmdDfcNJhxgJ1X86XZwW8sYfCODQX0';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, 'Received your message');
+});
