8be837e279167870029a791da090bb3acb9529c3
PC <mixail.ermin33@gmail.com>
2024-04-07 23:21:44 +0300

    tonnconnect

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { TonConnectButton } from '@tonconnect/ui-react'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +29,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>
+            <span>Buy Tons with React UI</span>
+            <TonConnectButton />
+      </div>
     </>
   )
 }
--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {TonConnectUIProvider} from "@tonconnect/ui-react";
+
+const manifestUrl = "./tonconnect-manifest.json";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </TonConnectUIProvider>,
 )
