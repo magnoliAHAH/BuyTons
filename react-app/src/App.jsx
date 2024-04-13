43ae94eb62cb0c4284f0324bb6af3ff3febc3377
PC <mixail.ermin33@gmail.com>
2024-04-13 13:32:38 +0300

    clicker fix

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -4,19 +4,6 @@ import styles from './App.module.scss';
 export function App() {
   const [counter, setCounter] = useState(0);
 
-  // Fetch counter value from localStorage on component mount
-  useEffect(() => {
-    const savedCounter = localStorage.getItem('counter');
-    const parsedCounter = savedCounter !== null ? parseInt(savedCounter) : 0;
-    setCounter(parsedCounter);
-    console.log("Fetched counter value from localStorage:", parsedCounter);
-  }, []);
-
-  // Save counter value to localStorage on update
-  useEffect(() => {
-    localStorage.setItem('counter', counter);
-  }, [counter]);
-
   return (
     <div className={styles.BG}>
       <div className={styles.plate}>
