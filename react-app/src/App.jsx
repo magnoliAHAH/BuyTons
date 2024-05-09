cdd14b29d0c1e37ede67e8676cbb62bcae8fb35e
PC <mixail.ermin33@gmail.com>
2024-05-09 14:07:28 +0300

    tonconnect

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,15 +1,20 @@
 import { useState, useEffect } from 'react';
 import styles from './App.module.scss';
+import { TonConnectButton, TonConnectUIProvider } from '@tonconnect/ui-react';
+
 
 export function App() {
   const [counter, setCounter] = useState(0);
 
   return (
-    <div className={styles.BG}>
-      <div className={styles.plate}>
-        <counter className={styles.counter}>{counter}</counter>
+    <TonConnectUIProvider manifestUrl="https://mixail.ermin33.fvds.ru/tonconnect-manifest.json">
+      <div className={styles.BG}>
+        <div className={styles.plate}>
+          <counter className={styles.counter}>{counter}</counter>
+        </div>
+        <button onClick={() => setCounter(counter + 1)} className={styles.monsterButton}></button>
+        <TonConnectButton/>
       </div>
-      <button onClick={() => setCounter(counter + 1)} className={styles.monsterButton}></button>
-    </div>
+    </TonConnectUIProvider>
   );
 }
